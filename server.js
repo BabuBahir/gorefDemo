d5ddff473640d1d5955da210cc0adb72fd0e3290
Rohit <ofd.aws96@gmail.com>
2021-02-01 22:06:53 +0530

    added body-parser

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const express = require("express");
+var bodyParser = require('body-parser'); 
 const app = express();
 const employees = require("./model");
 const router = express.Router();
+const path = require('path');
 const port = 4000;
 
 var uri = "mongodb://localhost:27017/demo";
@@ -15,32 +17,31 @@ connection.once("open", function() {
   console.log("MongoDB database connection established successfully");
 });
 
+// configure the app to use bodyParser()
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 app.use("/", router);
+ 
 
 app.listen(port, function() {
   console.log("Server is running on Port: " + port);
 });
 
 
-	
-	var data = [
-	  {
-		name: "John",
-		location: "New York"
-	  },
-	  {
-		name: "Smith",	
-		location: "Texas"
-	  },
-	  {
-		name: "Lisa",		
-		location: "Chicago"
-	  }
-	];
 
 
+router.route("/").get(function(req, res) {	  
+	  res.sendFile(path.join(__dirname + '/index.html'));
+});
+ 
  
 router.route("/insertdata").post(function(req, res) {
+	var body  =req.body;	 	
+	
+	var data = [{ name: body["fullName"] ,location: body["location"] }];	 
+	 
 	employees.insertMany(data, function(err, result) {
 	  if (err) {
 		res.send(err);
