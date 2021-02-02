c1d43e3a2822bc9e15d1b879c6be9578255fa0eb
Rohit <ofd.aws96@gmail.com>
2021-02-02 15:39:11 +0530

    always res.redirect("/list")

--- server.js
+++ server.js
@@ -31,97 +31,85 @@ app.listen(port, function() {
 
 
 
-router.route("/").get(function(req, res) {	  
-	  res.sendFile(path.join(__dirname + '/index.html'));
-});
+		router.route("/").get(function(req, res) {	  
+			  res.sendFile(path.join(__dirname + '/index.html'));
+		});
 
-router.route("/list").get(function(req, res) {	  
-	  res.sendFile(path.join(__dirname + '/list.html'));
-}); 
- 
-router.route("/insertdata").post(function(req, res) {
-	var body  =req.body;	 	
-	
-	var data = [{ name: body["fullName"] ,location: body["location"] }];	 
-	 
-	employees.insertMany(data, function(err, result) {
-	  if (err) {
-		res.send(err);
-	  } else {
-		  res.redirect("/list"); 		   
-		//res.send(result);
-	  }
-	});
-});
-
-
-router.route("/fetchdata").get(function(req, res) {
-  employees.find({}, function(err, result) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
+		router.route("/list").get(function(req, res) {	  
+			  res.sendFile(path.join(__dirname + '/list.html'));
+		}); 
+		 
+		router.route("/insertdata").post(function(req, res) {
+			var body  =req.body;	 	
+			
+			var data = [{ name: body["fullName"] ,location: body["location"] }];	 
+			 
+			employees.insertMany(data, function(err, result) {
+			  if (err) {
+				res.send(err);
+			  } else {
+				  res.redirect("/list"); 		   
+				//res.send(result);
+			  }
+			});
+		});
 
 
-router.route("/find").get(function(req, res) {
-	
-		var name="pap"; 
-		employees.find({name: {$regex: name, $options: 'i'}}, function (err, doc) { 
-		if (err){ 
-			console.log(err); 
-		} 
-		else{ 
-			console.log("Second function call : ", doc); 
-			res.send(docs);
-		} 
-	}); 
- 
-});
+		router.route("/fetchdata").get(function(req, res) {
+		  employees.find({}, function(err, result) {
+			if (err) {
+			  res.send(err);
+			} else {
+			  res.send(result);
+			}
+		  });
+		});
 
 
-router.route("/edit/:id").get(function(req, res) {
-	
-		var id=req.params.id;  
-		employees.findOne({_id: id}, function (err, doc) { 
-		if (err){ 
-			console.log(err); 
-		} 
-		else{ 			 
-			res.send(doc);
-		} 
-	});  
-});
-
-router.route("/delete/:id").get(function(req, res) {
-	
-		var id=req.params.id;  
-		employees.findOne({_id: id}, function (err, doc) { 
-		if (err){ 
-			console.log(err); 
-		} 
-		else{ 						 
-			doc.remove();    // to delete
-			res.redirect("/list"); 
-		} 
-	});  
-});
+		router.route("/find/:id").get(function(req, res) {
+			
+				// var name="pap"; 
+				// employees.find({name: {$regex: name, $options: 'i'}}, function (err, doc) { 
+					// if (err){ 
+						// console.log(err); 
+					// } 
+					// else{ 
+						// console.log("Second function call : ", doc); 
+						// res.send(docs);
+					// } 
+				// }); 
+				
+				var id=req.params.id;  
+				employees.findOne({_id: id}, function (err, doc) { 
+				if (err){ 
+					console.log(err); 
+				} 
+				else{ 			 
+					res.send(doc);
+				}
+			 
+			});
+		});	
 
 
+		router.route("/edit/:id").get(function(req, res) { 
+				var id=req.params.id;  
+				res.sendFile(path.join(__dirname + '/edit.html'	)); 
+		});
 
-
-
-
-
-
-
-
-
-
-
-
+		router.route("/delete/:id").get(function(req, res) {
+			
+				var id=req.params.id;  
+				employees.findOne({_id: id}, function (err, doc) { 
+				if (err){ 
+					console.log(err); 
+				} 
+				else{ 						 
+					doc.remove();    // to delete
+					res.redirect("/list"); 
+				} 
+			});  
+		});
 
 
 
